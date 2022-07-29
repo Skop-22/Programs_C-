54f02ece235163a2de10c6d4fcd4fa7f9ec06e8a
Skop-22 <100052537+Skop-22@users.noreply.github.com>
2022-07-29 15:46:18 -0500

    Delete HTML directory

--- HTML/EnlaceBD.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //mysqli_conect srive para conectar un servidor 
-    //mariadb o mysql y a una base de datos del mismo
-    $enlace=mysqli_connect("localhost","root","","ico6");
-    if(!$enlace){
-        die("Error de conexion");
-    }
-?>
--- HTML/index.php
+++
@@ -1,9 +0,0 @@
-<html lang="en">
-<head>
-    <?php require("cabeceras.php"); ?>
-</head>
-    <body>
-        <?php require("menu.php"); ?>
-        <h1>Pagina principal</h1>
-    </body>
-</html>
